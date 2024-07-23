61a616555c870069bfde97abf83e001561412fee
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-07-23 20:20:34 +0900

    [ ✨ | #2 | Feat ] 배포세팅

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/main">
+      <BrowserRouter>
         <Routes />
       </BrowserRouter>
     </>
